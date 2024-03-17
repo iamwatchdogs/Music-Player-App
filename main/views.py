78478ba169ebd10bf55cf7eadf8cbce63e73edcf
iamwatchdogs <shamith3011022@gmail.com>
2024-03-17 19:34:43 +0530

    Linked the template the view

--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def sample_view(request):
-	return HttpResponse("<h1>Sample View</h1>")
+def index(request):
+	return render(request, 'index.html', {})
